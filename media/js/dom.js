32dfa2868f5db1bec1801cfc00ca5c1dd7fa4e74
Satanpit <alex.hyrenko@gmail.com>
2014-12-13 22:35:34 +0200

    DOM JS: update structore

--- media/js/dom.js
+++ media/js/dom.js
@@ -52,7 +52,7 @@ var DOM = (function() {
      * @param selector
      * @constructor
      */
-    function Element(selector) {
+    function Elements(selector) {
         var elements;
 
         if (isElementsList(selector)) {
@@ -72,161 +72,166 @@ var DOM = (function() {
         }.bind(this));
     }
 
-    Object.defineProperties(Element.prototype, {
+    Object.defineProperties(Elements.prototype, {
         length: {
-            get: function() {
+            get: function () {
                 return Object.keys(this).length;
-            }
-        },
-
-        each: {
-            value: function(callback) {
-                [].forEach.call(this, callback);
-                return this;
-            }
-        },
-
-        click: {
-            value: function(callback) {
-                return this.each(function(item) {
-                    item.addEventListener('click', callback);
-                });
-            }
-        },
-
-        show: {
-            value: function () {
-                return this.each(function(item) {
-                    item.style.display = 'block';
-                });
-            }
-        },
-
-        hide: {
-            value: function () {
-                return this.each(function(item) {
-                    item.style.display = 'none';
-                });
-            }
-        },
-
-        hasClass: {
-            value: function(name) {
-                return this[0].classList.contains(name)
-            }
-        },
-
-        addClass: {
-            value: function(name) {
-                return this.each(function(item) {
-                    item.classList.add(name);
-                });
-            }
-        },
-
-        removeClass: {
-            value: function(name) {
-                return this.each(function(item) {
-                    item.classList.remove(name);
-                });
-            }
-        },
-
-        toggleClass: {
-            value: function(name) {
-                return this.each(function(item) {
-                    item.classList.toggle(name);
-                });
-            }
-        },
-
-        attr: {
-            value: function(key, value) {
-                if (isString(value)) {
-                    return this.each(function(item) {
-                        item.setAttribute(key, value);
-                    });
-                }
-
-                return this[0].getAttribute(key);
-            }
-        },
-
-        data: {
-            value: function(key, value) {
-                if (isString(value) || isObject(key)) {
-                    return this.each(function(item) {
-                        isObject(key) && each(key, function(name, val) {
-                            item.dataset[name] = val;
-                        });
-
-                        isString(value) && (item.dataset[key] = value);
-                    });
-                }
-
-                return this[0].dataset[key];
-            }
-        },
-
-        append: {
-            value: function(html) {
-                return this.each(function(item) {
-                    item.insertAdjacentHTML('beforeend', html);
-                });
-            }
-        },
-
-        after: {
-            value: function(html) {
-                return this.each(function(item) {
-                    item.insertAdjacentHTML('afterend', html);
-                });
-            }
-        },
-
-        prepend: {
-            value: function(html) {
-                return this.each(function(item) {
-                    item.insertAdjacentHTML('afterbegin', html);
-                });
-            }
-        },
-
-        before: {
-            value: function(html) {
-                return this.each(function(item) {
-                    item.insertAdjacentHTML('beforebegin', html);
-                });
-            }
-        },
-
-        text: {
-            value: function(text) {
-                if (isString(text)) {
-                    return this.each(function(item) {
-                        item.innerText = text;
-                    });
-                }
-
-                return this[0].innerHTML;
-            }
-        },
-
-        html: {
-            value: function(html) {
-                if (isString(html)) {
-                    return this.each(function(item) {
-                        item.innerHTML = html;
-                    })
-                }
-
-                return this[0].innerHTML;
             }
         }
     });
 
+    Elements.extend = function(properties) {
+        var proto = { };
+        each(properties, function(key, item) {
+            proto[key] = { value: item }
+        });
+
+        return Object.defineProperties(this.prototype, proto);
+    };
+
+    Elements.extend({
+        /**
+         * Each elements
+         * @param {Function} callback
+         * @returns {Elements}
+         */
+        each: function(callback) {
+            [].forEach.call(this, callback);
+            return this;
+        },
+
+        /**
+         * Inner or get element text
+         * @param {String} text
+         * @returns {Elements}
+         */
+        text: function(text) {
+            if (isString(text)) {
+                return this.each(function(item) {
+                    item.innerText = text;
+                });
+            }
+
+            return this[0].innerHTML;
+        },
+
+        html: function(html) {
+            if (isString(html)) {
+                return this.each(function(item) {
+                    item.innerHTML = html;
+                })
+            }
+
+            return this[0].innerHTML;
+        },
+
+        parent: function() {
+
+        },
+
+        parents: function(selector) {
+
+        },
+
+        find: function(selector) {
+
+        },
+
+        eq: function(index) {
+
+        },
+
+        click: function(callback) {
+            return this.each(function(item) {
+                item.addEventListener('click', callback);
+            });
+        },
+
+        append: function(html) {
+            return this.each(function(item) {
+                item.insertAdjacentHTML('beforeend', html);
+            });
+        },
+
+        after: function(html) {
+            return this.each(function(item) {
+                item.insertAdjacentHTML('afterend', html);
+            });
+        },
+
+        prepend: function(html) {
+            return this.each(function(item) {
+                item.insertAdjacentHTML('afterbegin', html);
+            });
+        },
+
+        before: function(html) {
+            return this.each(function(item) {
+                item.insertAdjacentHTML('beforebegin', html);
+            });
+        },
+
+        attr: function(key, value) {
+            if (isString(value)) {
+                return this.each(function(item) {
+                    item.setAttribute(key, value);
+                });
+            }
+
+            return this[0].getAttribute(key);
+        },
+
+        data: function(key, value) {
+            if (isString(value) || isObject(key)) {
+                return this.each(function(item) {
+                    isObject(key) && each(key, function(name, val) {
+                        item.dataset[name] = val;
+                    });
+
+                    isString(value) && (item.dataset[key] = value);
+                });
+            }
+
+            return this[0].dataset[key];
+        },
+
+        hasClass: function(name) {
+            return this[0].classList.contains(name);
+        },
+
+        addClass: function(name) {
+            return this.each(function(item) {
+                item.classList.add(name);
+            });
+        },
+
+        removeClass: function(name) {
+            return this.each(function(item) {
+                item.classList.remove(name);
+            });
+        },
+
+        toggleClass: function(name) {
+            return this.each(function(item) {
+                item.classList.toggle(name);
+            });
+        },
+
+        show: function () {
+            return this.each(function(item) {
+                item.style.display = 'block';
+            });
+        },
+
+        hide: function () {
+            return this.each(function(item) {
+                item.style.display = 'none';
+            });
+        }
+    });
+
     return function(selector) {
-        return new Element(selector);
+        return new Elements(selector);
     };
 }());
 
