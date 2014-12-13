d370f2ef56b219b4ae7899cfcae78ed268d8006c
Satanpit <alex.hyrenko@gmail.com>
2014-12-13 19:48:10 +0200

    add dom framework by Xss

--- media/js/dom.js
+++ media/js/dom.js
@@ -1,15 +1,62 @@
 var DOM = (function() {
+    "use strict";
+
+    function isArray(array) {
+        return Array.isArray(array);
+    }
+
+    function isObject(object) {
+        return typeof object === 'object' ? !isArray(object) : false;
+    }
+
+    function isFunction(func) {
+        return typeof func === 'function';
+    }
+
+    function isString(str) {
+        return typeof str === 'string';
+    }
+
+    function isElement(element) {
+        return element instanceof Element;
+    }
+
+    function isElementsList(list) {
+        return list instanceof HTMLCollection;
+    }
+
+    function isNode(object) {
+        return object instanceof Node;
+    }
 
     function Element(selector) {
-        var element = document.querySelectorAll(selector);
-        this.length = element.length;
+        var elements;
 
-        [].forEach.call(element, function(item, index) {
+        if (isElementsList(selector)) {
+            elements = selector;
+        }
+        else if (isNode(selector)) {
+            this[0] = selector;
+            return ;
+        }
+        else if (isString(selector)) {
+            elements = document.querySelectorAll(selector);
+
+        } else throw Error('Vania dolboeb!');
+
+        [].forEach.call(elements, function(item, index) {
             this[index] = item;
         }.bind(this));
     }
 
     Object.defineProperties(Element.prototype, {
+
+        length: {
+            get: function() {
+                return Object.keys(this).length;
+            }
+        },
+
         each: {
             value: function(callback) {
                 [].forEach.call(this, callback);
@@ -44,12 +91,22 @@ var DOM = (function() {
 
                 return this;
             }
+        },
+
+        html: {
+            value: function(html) {
+                return isString(html) ? (this[0].innerHTML = html) && this : this[0].innerHTML;
+            }
         }
     });
 
-    return {
-        get: function(selector) {
-            return new Element(selector);
-        }
-    }
+    return function(selector) {
+        return new Element(selector);
+    };
 }());
+
+DOM.ready = function(callback) {
+    if (typeof callback === 'function') {
+        document.addEventListener('DOMContentLoaded', callback, false);
+    }
+};
